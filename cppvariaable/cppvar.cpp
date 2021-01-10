d5d7ef9ab553834434c6b183cc51ed398b3a598b
boringman <boringmanzr@outlook.com>
2021-01-10 16:25:58 +0800

    add cpp var change mode test

--- cppvariaable/cppvar.cpp
+++ cppvariaable/cppvar.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
 // 变量声明   
-extern int a,b;    
+/*
+C/C++ 编译 cpp 文件是从上往下编译，所以 main 函数里面调用其他函数时，
+如果其他函数在 main 函数的下面，则要在 main 函数上面先声明这个函数。
+或者把 main 函数放在最下面，
+这个不仅限于 main 函数，其他函数的调用都是如此。
+被调用的函数要在调用的函数之前声明。
+*/
+extern int a,b;     //声明不包含变量，只是声明了有一个变量a存在，具体a在哪里定义的，需要编译器编译的时候找到。
 extern int c;
 extern float f;
+int d = 1;            //定义并声明变量d
 
 int main()
 {
