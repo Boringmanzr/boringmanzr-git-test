1988bf6efd316dfdaa05f2267542c876cffe2076
boringman <boringmanzr@outlook.com>
2021-01-10 15:04:00 +0800

    change 注释 add cpp path

--- cppdatetype/cppdate.cpp
+++ cppdatetype/cppdate.cpp
@@ -5,9 +5,8 @@ using namespace std;
 
 int main()
 {
-    cout << "type : \t\t" << "***************size**************"<< endl;    // “\t”表示水平制表符，跳到下一个Tab的位置
-    cout << "bool : \t\t" << "所占字节数：" << sizeof(bool);   
-    cout << "\t 最大值：" << (numeric_limits<bool>::max)();
-    cout << "\t\t 最小值：" << (numeric_limits<bool>::min)();
-    return 0;
+    cout << "type : \t\t" << "***************size**************"<< endl;   // “\t”表示水平制表符，跳到下一个Tab的位置
+    cout << "bool : \t\t" << "所占字节数：" << sizeof(bool);                //sizeof()函数用来获取各种数据类型的大小 
+    cout << "\t 最大值：" << (numeric_limits<bool>::max)();                 //<<运算符用于向屏幕传多个值
+    cout << "\t\t 最小值：" << (numeric_limits<bool>::min)()<< endl;        // 使用endl将在每一行插入一个换行符
 }
