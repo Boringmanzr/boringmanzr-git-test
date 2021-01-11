13f63bbf53d1de39578b7fa10e37a344502426db
boringman <boringmanzr@outlook.com>
2021-01-11 19:19:49 +0800

    add cpp modifier test

---
+++ cppmodifier/cpp-modify.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short int a;
+    short unsigned int b;
+    b = 5000;
+    a = b;
+    cout << a << endl;
+    cout << b << endl;
+    return 0;
+} 
